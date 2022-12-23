64616a8012742ac23f34b2f25ceb3f4bb81cfdc0
Dariusz Skiciak <dskiciak@gmail.com>
2022-12-23 22:56:17 +0100

    test: add test to cards controller

--- src/cards/services/cards.service.ts
+++ src/cards/services/cards.service.ts
@@ -3,7 +3,7 @@ import { TemplateDto } from '../dto/Template.dto';
 
 @Injectable()
 export class CardsService {
-    getTemplates(): TemplateDto[] {
-        return [{ name: 'test', values: [1, 2] }];
+    async getTemplates(): Promise<TemplateDto[]> {
+        return Promise.resolve([]);
     }
 }
