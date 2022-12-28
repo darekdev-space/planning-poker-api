aa7355c9bfa87e45e68e5a559d161c779b115582
Dariusz Skiciak <dskiciak@gmail.com>
2022-12-28 22:10:36 +0100

    feat: init typeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CardsModule } from './cards/cards.module';
 
 @Module({
-    imports: [CardsModule],
+    imports: [
+        CardsModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'admin',
+            password: 'admin',
+            database: 'data',
+            entities: [],
+            synchronize: true,
+        }),
+    ],
     controllers: [],
     providers: [],
 })
